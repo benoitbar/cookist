3bff58738ecfe775fca8393f6a797e3ef943620d
Benoit Bar <benoit.bar@gandi.net>
2023-08-01 21:31:17 +0200

    Make the app a PWA

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { createRoot } from 'react-dom/client';
+
 import App from './App';
+
+// Call the element loader after the platform has been bootstrapped
+defineCustomElements(window);
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
