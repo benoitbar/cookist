dbd166730737d2d933e51729799ec2776ff1ab30
Benoit Bar <benoit.bar@gandi.net>
2024-05-30 17:05:40 +0200

    Clean unused code

--- src/pages/RecipeList.tsx
+++ src/pages/RecipeList.tsx
@@ -23,24 +23,10 @@ interface Props
 export const RecipeList: React.FC<Props> = () => {
   const data = useQuery(api.recipes.getCollection);
   const create = useMutation(api.recipes.create);
-  const update = useMutation(api.recipes.update);
-  const remove = useMutation(api.recipes.remove);
 
   async function handleCreateRecipe(value: string) {
     const name = value.trim();
-    await create({ name: value, unit: 4 });
-  }
-
-  async function handleUpdateRecipe(
-    oldItem: Doc<'recipes'>,
-    newItem: Doc<'recipes'> | void
-  ) {
-    if (newItem) {
-      const { _creationTime, ...data } = newItem;
-      await update(data);
-    } else {
-      await remove({ _id: oldItem._id });
-    }
+    await create({ name, unit: 4 });
   }
 
   return (
--- src/pages/ShoppingList.tsx
+++ src/pages/ShoppingList.tsx
@@ -16,24 +16,10 @@ import { ShoppingItem } from '../components/ShoppingItem';
 export const ShoppingList: React.FC = () => {
   const data = useQuery(api.shopping.getCollection);
   const create = useMutation(api.shopping.create);
-  const update = useMutation(api.shopping.update);
-  const remove = useMutation(api.shopping.remove);
 
   async function handleCreateItem(value: string) {
     const name = value.trim();
     await create({ name });
-  }
-
-  async function handleUpdateItem(
-    oldItem: Doc<'shopping'>,
-    newItem: Doc<'shopping'> | void
-  ) {
-    if (newItem) {
-      const { _creationTime, ...data } = newItem;
-      await update(data);
-    } else {
-      await remove({ _id: oldItem._id });
-    }
   }
 
   return (
